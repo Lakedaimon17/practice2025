20c81c29876f92e9c21570bf745e2cc7840e5a1e
Lakedaimon <dkd1717301@gmail.com>
2025-06-30 21:10:20 +0600

    feat: ISpaceship

---
+++ task04/Class1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace task04
+{
+    public interface ISpaceship
+    {
+        void MoveForward();
+        void Rotate(int angle);
+        void Fire();
+        int Speed { get; }
+        int FirePower { get; }
+    }
+
+    public class Cruiser : ISpaceship
+    {
+        public int Coordinates { get; private set; } = 0;
+        public int RotationAngle { get; private set; } = 0;
+        public int Projectiles { get; private set; } = 150;
+        public int Speed { get; } = 50;
+        public int FirePower { get; } = 100;
+        public int Damage { get; private set; } = 0;
+
+        public void MoveForward() => Coordinates += Speed;
+
+        public void Rotate(int angle) => RotationAngle = (RotationAngle + angle) % 360;
+
+        public void Fire()
+        {
+            if (Projectiles > 0)
+            {
+                Damage += FirePower;
+                Projectiles--;
+            }
+        }
+    }
+
+    public class Fighter : ISpaceship
+    {
+        public int Coordinates { get; private set; } = 0;
+        public int RotationAngle { get; private set; } = 0;
+        public int Projectiles { get; private set; } = 50;
+        public int Speed { get; } = 100;
+        public int FirePower { get; } = 40;
+        public int Damage { get; private set; } = 0;
+
+        public void MoveForward() => Coordinates += Speed;
+
+        public void Rotate(int angle) => RotationAngle = (RotationAngle + angle) % 360;
+
+        public void Fire()
+        {
+            if (Projectiles > 0)
+            {
+                Damage += FirePower;
+                Projectiles--;
+            }
+        }
+    }
+}
---
+++ task04tests/UnitTest1.cs
@@ -0,0 +1,109 @@
+﻿using Xunit;
+using task04;
+
+namespace task04tests
+{
+    public class SpaceshipTests
+    {
+        [Fact]
+        public void Cruiser_ShouldHaveCorrectStats()
+        {
+            var cruiser = new Cruiser();
+            Assert.Equal(0, cruiser.Coordinates);
+            Assert.Equal(0, cruiser.RotationAngle);
+            Assert.Equal(150, cruiser.Projectiles);
+            Assert.Equal(50, cruiser.Speed);
+            Assert.Equal(100, cruiser.FirePower);
+            Assert.Equal(0, cruiser.Damage);
+        }
+
+        [Fact]
+        public void Fighter_ShouldHaveCorrectStats()
+        {
+            var fighter = new Fighter();
+            Assert.Equal(0, fighter.Coordinates);
+            Assert.Equal(0, fighter.RotationAngle);
+            Assert.Equal(50, fighter.Projectiles);
+            Assert.Equal(100, fighter.Speed);
+            Assert.Equal(40, fighter.FirePower);
+            Assert.Equal(0, fighter.Damage);
+        }
+
+        [Fact]
+        public void Fighter_ShouldBeFasterThanCruiser()
+        {
+            var fighter = new Fighter();
+            var cruiser = new Cruiser();
+            Assert.True(fighter.Speed > cruiser.Speed);
+        }
+
+        [Fact]
+        public void Cruiser_ShouldBeStrongerThanFighter()
+        {
+            var fighter = new Fighter();
+            var cruiser = new Cruiser();
+            Assert.True(cruiser.FirePower > fighter.FirePower);
+        }
+
+        [Fact]
+        public void Cruiser_MovesForwardCorrect()
+        {
+            var cruiser = new Cruiser();
+            int initialCoordinate = cruiser.Coordinates;
+            cruiser.MoveForward();
+            Assert.Equal(initialCoordinate + cruiser.Speed, cruiser.Coordinates);
+        }
+
+        [Fact]
+        public void Cruiser_RotatesCorrect()
+        {
+            var cruiser = new Cruiser();
+            int initialRotation = cruiser.RotationAngle;
+            cruiser.Rotate(50);
+            Assert.Equal(50, cruiser.RotationAngle);
+            cruiser.Rotate(50);
+            Assert.Equal(100, cruiser.RotationAngle);
+        }
+
+        [Fact]
+        public void Cruiser_FiresCorrect()
+        {
+            var cruiser = new Cruiser();
+            int initialProjectiles = cruiser.Projectiles;
+            int initialDamage = cruiser.Damage;
+            cruiser.Fire();
+            Assert.Equal(initialProjectiles - 1, cruiser.Projectiles);
+            Assert.Equal(initialDamage + cruiser.FirePower, cruiser.Damage);
+        }
+
+        [Fact]
+        public void Fighter_MovesForwardCorrect()
+        {
+            var fighter = new Fighter();
+            int initialCoordinate = fighter.Coordinates;
+            fighter.MoveForward();
+            Assert.Equal(initialCoordinate + fighter.Speed, fighter.Coordinates);
+        }
+
+        [Fact]
+        public void Fighter_RotatesCorrect()
+        {
+            var fighter = new Fighter();
+            fighter.Rotate(50);
+            Assert.Equal(50, fighter.RotationAngle);
+            fighter.Rotate(360);
+            Assert.Equal(50, fighter.RotationAngle);
+        }
+
+        [Fact]
+        public void Fighter_FiresCorrect()
+        {
+            var fighter = new Fighter();
+            int initialProjectiles = fighter.Projectiles;
+            int initialDamage = fighter.Damage;
+            fighter.Fire();
+            Assert.Equal(initialProjectiles - 1, fighter.Projectiles);
+            Assert.Equal(initialDamage + fighter.FirePower, fighter.Damage);
+        }
+    }
+}
