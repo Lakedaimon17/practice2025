b6e2232c7880fef0c3aea70c42e3b39a5e47e2f8
Lakedaimon <dkd1717301@gmail.com>
2025-07-15 14:00:07 +0600

    refactory: better usage of threads

--- task14/Class1.cs
+++ task14/Class1.cs
@@ -2,7 +2,7 @@
 using System.Linq;
 using System.Threading;
 
-public static class task14
+public static class DefiniteIntegral
 {
     public static double Solve(double a, double b, Func<double, double> function, double step, int threadsNumber)
     {
@@ -13,31 +13,45 @@ public static class task14
         if (totalSteps == 0) return 0;
 
         double h = length / totalSteps;
-        threadsNumber = Math.Clamp(threadsNumber, 1, totalSteps);
+        threadsNumber = Math.Min(threadsNumber, totalSteps);
 
-        long resultBits = 0;
+        var partialResults = new double[threadsNumber];
+        var barrier = new Barrier(threadsNumber + 1);
 
         Enumerable.Range(0, threadsNumber)
-            .AsParallel()
-            .WithDegreeOfParallelism(threadsNumber)
-            .Select(localId =>
+            .Select(threadIndex => new Thread(() =>
             {
-                int stepsPerThread = totalSteps / threadsNumber;
-                int remainder = totalSteps % threadsNumber;
-                int startStep = localId * stepsPerThread + Math.Min(localId, remainder);
-                int endStep = startStep + stepsPerThread + (localId < remainder ? 1 : 0);
+                var (startStep, endStep) = GetStepsRange(threadIndex, threadsNumber, totalSteps);
+                partialResults[threadIndex] = RecursiveSum(a, h, function, startStep, endStep);
+                barrier.SignalAndWait();
+            }))
+            .ToList()
+            .ForEach(thread => thread.Start());
 
-                return Enumerable.Range(startStep, endStep - startStep)
-                    .Select(j => (function(a + j * h) + function(a + (j + 1) * h)) * h / 2)
-                    .Sum();
-            })
-            .Aggregate(0.0, (acc, localSum) =>
-            {
-                long newValue = BitConverter.DoubleToInt64Bits(acc + localSum);
-                Interlocked.Exchange(ref resultBits, newValue);
-                return BitConverter.Int64BitsToDouble(resultBits);
-            });
+        barrier.SignalAndWait();
+        return partialResults.Sum();
+    }
 
-        return BitConverter.Int64BitsToDouble(resultBits);
+    private static (int startStep, int endStep) GetStepsRange(int threadIndex, int threadsNumber, int totalSteps)
+    {
+        int stepsPerThread = totalSteps / threadsNumber;
+        int remainder = totalSteps % threadsNumber;
+        int startStep = threadIndex * stepsPerThread + Math.Min(threadIndex, remainder);
+        int endStep = startStep + stepsPerThread + (threadIndex < remainder ? 1 : 0);
+        return (startStep, endStep);
+    }
+
+    private static double RecursiveSum(double a, double h, Func<double, double> function, int start, int end)
+    {
+        if (start + 1 >= end)
+        {
+            double x1 = a + start * h;
+            double x2 = a + end * h;
+            return (function(x1) + function(x2)) * h / 2;
+        }
+
+        int mid = (start + end) / 2;
+        return RecursiveSum(a, h, function, start, mid) +
+               RecursiveSum(a, h, function, mid, end);
     }
 }
--- task14tests/UnitTest1.cs
+++ task14tests/UnitTest1.cs
@@ -3,26 +3,26 @@
 public class task14tests
 {
     [Fact]
-    public void Test_Linear_Function()
+    public void Test_Linear_Function_Symmetric_Interval()
     {
-        Func<double, double> linear = x => x;
-        double result = task14.Solve(-1, 1, linear, 1e-4, 2);
-        Assert.InRange(result, -1e-4, 1e-4);
+        var linear = (double x) => x;
+        double result = DefiniteIntegral.Solve(-1, 1, linear, 1e-4, 2);
+        Assert.Equal(0, result, 1e-4);
     }
 
     [Fact]
-    public void Test_Sin_Function()
+    public void Test_Sin_Function_Symmetric_Interval()
     {
-        Func<double, double> sin = x => Math.Sin(x);
-        double result = task14.Solve(-1, 1, sin, 1e-5, 8);
-        Assert.InRange(result, -1e-4, 1e-4);
+        var sin = (double x) => Math.Sin(x);
+        double result = DefiniteIntegral.Solve(-1, 1, sin, 1e-5, 8);
+        Assert.Equal(0, result, 1e-4);
     }
 
     [Fact]
-    public void Test_Quadratic_Function()
+    public void Test_Linear_Function_From_0_To_5()
     {
-        Func<double, double> quadratic = x => x;
-        double result = task14.Solve(0, 5, quadratic, 1e-6, 8);
-        Assert.Equal(12.5, result, 5);
+        var linear = (double x) => x;
+        double result = DefiniteIntegral.Solve(0, 5, linear, 1e-6, 8);
+        Assert.Equal(12.5, result, 1e-5);
     }
 }
